33e52d0caa12d991cd1d1e18bb0b01356714c966
Thanatat Tamtan <acoshift@gmail.com>
2023-12-11 17:04:39 +0700

    allow push perm to pull (#1)
    allow user who have push permission to do pull operation, since push
    operation need to check is blobs already exists

--- src/auth.js
+++ src/auth.js
@@ -37,52 +37,23 @@ export async function authorized (request, env, ctx) {
 	}
 	request.namespace = project
 
-	const perm = isPushRequest(request)
-		? pushPermission
-		: pullPermission
-
-	const cache = caches.default
-	const cacheKey = `deploys--registry|auth|${project}|${perm}|${auth}`
-	const cacheReq = new Request(authEndpoint, {
-		cf: {
-			cacheTtl: 30,
-			cacheKey,
-			cacheTags: ['deploys--registry|auth']
-		}
-	})
-	let resp = await cache.match(cacheReq)
-	if (!resp) {
-		resp = await fetch(authEndpoint, {
-			method: 'POST',
-			headers: {
-				authorization: auth,
-				'content-type': 'application/json'
-			},
-			body: JSON.stringify({
-				project,
-				permissions: [perm]
-			})
-		})
-		if (!resp.ok) {
-			return unauthorizedResponse
-		}
-
-		// cache
-		const cacheResp = new Response(resp.clone().body, resp)
-		cacheResp.headers.set('cache-control', 'public, max-age=30')
-		ctx.waitUntil(cache.put(cacheReq, cacheResp))
+	// push permission can do both push and pull
+	if (await checkPermission(auth, project, pushPermission, env, ctx)) {
+		return // authorized
 	}
 
-	const res = await resp.json()
-	if (!res.ok) {
+	// user don't have push permission
+	if (isPushRequest(request)) {
+		// so reject push request
 		return unauthorizedResponse
 	}
-	if (!res.result.authorized) {
-		return unauthorizedResponse
+
+	if (await checkPermission(auth, project, pullPermission, env, ctx)) {
+		return // authorized
 	}
-	if (!res.result.project.billingAccount.active) {
-		return unauthorizedResponse
-	}
+
+	// user don't have any permission
+	return unauthorizedResponse
 }
 
 /**
@@ -137,4 +108,48 @@ async function getEmail (auth, env, ctx) {
 		return ''
 	}
 	return res.result.email
+}
+
+/**
+ * @param {string} auth
+ * @param {string} project
+ * @param {string} permission
+ * @param {Env} env
+ * @param {import('@cloudflare/workers-types').ExecutionContext} ctx
+ * @returns {Promise<boolean>}
+ */
+async function checkPermission (auth, project, permission, env, ctx) {
+	const cache = caches.default
+	const cacheKey = `deploys--registry|auth|${project}|${permission}|${auth}`
+	const cacheReq = new Request(authEndpoint, {
+		cf: {
+			cacheTtl: 30,
+			cacheKey,
+			cacheTags: ['deploys--registry|auth']
+		}
+	})
+	let resp = await cache.match(cacheReq)
+	if (!resp) {
+		resp = await fetch(authEndpoint, {
+			method: 'POST',
+			headers: {
+				authorization: auth,
+				'content-type': 'application/json'
+			},
+			body: JSON.stringify({
+				project,
+				permissions: [permission]
+			})
+		})
+		if (!resp.ok) {
+			return false
+		}
+
+		const cacheResp = new Response(resp.clone().body, resp)
+		cacheResp.headers.set('cache-control', 'public, max-age=30')
+		ctx.waitUntil(cache.put(cacheReq, cacheResp))
+	}
+
+	const res = await resp.json()
+	return res.ok && res.result.authorized && res.result.project.billingAccount.active
 }
